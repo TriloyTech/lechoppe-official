4be911861b2f984d3126fca15c43ad1d40ffa557
Jamael Tanveer <imjtnayon@gmail.com>
2026-07-23 22:33:45 +0200

    feat(reservation): add extra lunch (14:30, 15:00) and dinner (22:30, 23:00) time slots

--- components/ReservationSection.tsx
+++ components/ReservationSection.tsx
@@ -5,8 +5,8 @@ import { useLang } from "@/context/LangContext";
 import BotCheck from "@/components/BotCheck";
 import PromotionModal from "@/components/PromotionModal";
 
-const SLOTS_LUNCH = ["12:00", "12:30", "13:00", "13:30", "14:00"];
-const SLOTS_DINNER = ["19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00"];
+const SLOTS_LUNCH = ["12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00"];
+const SLOTS_DINNER = ["19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00"];
 
 type Step = 0 | 1 | 2 | 3 | 4; // Date, Guests, Time, Contact, Success
 
